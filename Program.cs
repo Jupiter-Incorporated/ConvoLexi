73636faf2128b578e5fd1a85278b84cd61c6bfea
GamerZingerBurger1 <91785895+GamerZingerBurger1@users.noreply.github.com>
2022-01-07 22:35:18 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConvoLexi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "ConvoLexi";
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WindowHeight = 40; 
+
+            Console.WriteLine("Hello, Freind What Is Your Name?");
+
+            Console.ReadLine(); 
+
+            Console.WriteLine("Cool My Name Is Lexi! It's So Great To Meet You");
+            Console.WriteLine("What Is Your Faviorate Colour? I Need This Info To Help With Future Tasks");
+
+            Console.ReadLine(); 
+
+            Console.WriteLine("That Is A Great Colour We Have Finished Setup");
+
+            Console.WriteLine("I Do Not Know That Much As Of Now But I Will Try My Best To Help");
+
+            Console.ReadLine(); 
+            
+            Console.WriteLine("I Am Not Sure Of This Yet ");
+
+            Console.ReadLine();
+
+            Console.WriteLine("I Am Not Sure Of This Yet ");
+
+            Console.ReadKey();
+        }
+    }
+}
